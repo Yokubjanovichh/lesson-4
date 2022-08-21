3155ab50f29f7869ea79f6a833f137c4eeff244d
Murodulla <yokubjanovich@gmail.com>
2022-08-21 22:23:18 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+let oy = 5;
+
+switch (oy) {
+  case 12: case 11: case 1:
+    console.log("Qish");
+    break;
+  case 2: case 3: case 4:
+    console.log("Bahor");
+    break;
+  case 5: case 6: case 7:
+    console.log("Yoz");
+    break;
+  case 8: case 9: case 10:
+    console.log("Kuz");
+    break;
+  default:
+    console.log("noto'g'ri oy kuni kiritildi");
+}
